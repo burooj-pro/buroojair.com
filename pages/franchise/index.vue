1167c5665913c6f275515e7c84a9f89bd433da81
Mutlaq Aldhbuiub <matulkn@gmail.com>
2023-08-09 21:38:07 +0300

    Franchise Page (Without form)

---
+++ pages/franchise/index.vue
@@ -0,0 +1,313 @@
+<template>
+  <div>
+    <section class="standardSolar aboutSetting">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="text-box text-center">
+              <h3>
+                {{ $t("franchise") }}
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="content">
+      <div class="container">
+        <div class="row">
+          <p>
+            <b>Franchise model Burooj AIR</b>
+            <br/>
+            <b>NON-DISCLOSURE AGREEMENT</b>
+          </p>
+          <!-- First Section -->
+          <div class="col-6">
+            <p><b>THIS AGREEMENT (the NDA) is entered into between:</b></p>
+            <p>
+              (1) KTV Group AS, a private limited company incorporated under the
+              laws of Norway (registered no. 985 994 641) whose registered
+              office is at Hanøytangen 111, 5310 Hauglandshella, Askøy kommune
+              (Company); and (2) The visitor to this page and recipient of
+              information of the KTV Working Drone (the Recipient). together
+              referred to as the Parties and each as a Party to this NDA.
+            </p>
+            <p>
+              <b>BACKGROUND</b>
+              (A) The Company has developed a technology for utilizing drones in
+              the cleaning industry and is contemplating a collaboration (the
+              Collaboration) with the Recipient. The Company has therefore
+              invited the Recipient to discuss the possibility of a distribution
+              or resale agreement between the Parties. The Recipient will in
+              connection with such discussions be invited to a demonstration of
+              the drone technology. Furthermore, the Recipient will be provided
+              with certain Confidential Information (as defined in clause 1.1).
+              (B) This NDA sets out the terms and conditions agreed between the
+              Parties in relation to the provision of such Confidential
+              Information by the Company to the Recipient. (C) The Confidential
+              Information provided from the Company to the Recipient will be
+              provided on a non-exclusive basis, and this NDA does not restrict
+              the Company to give the same Confidential Information to any other
+              entities.
+            </p>
+            <p>
+              AGREED TERMS 1 Confidentiality 1.1 For the purpose of this NDA,
+              Confidential Information shall mean all information (however
+              recorded or preserved) which is or has been, either directly or
+              indirectly, disclosed or made available to the Recipient (in any
+              form or medium), in connection with the demonstration of the drone
+              technology and Recipient considering the Collaboration, and shall
+              also include any and all information regarding the existence,
+              nature or progress of any discussions between the Parties relating
+              to the possible Collaboration, including the demonstration of the
+              drone technology. 1.2 The Recipient shall keep all Confidential
+              Information strictly confidential and shall not disclose any
+              Confidential Information to any third party. 1.3 The Recipient may
+              disclose Confidential Information only when so required by law or
+              any court of competent jurisdiction, provided that the Company is
+              given due notice prior to such disclosure. 1.4 The Recipient may
+              disclose such Confidential Information as may be necessary to such
+              of its directors, officers, employees, and advisors (the
+              Representatives) when strictly necessary to enable the Recipient
+              to evaluate, prepare for, negotiate and carry out the
+              Collaboration. Prior to any such disclosure of Confidential
+              Information, the Recipient shall ensure that each such
+              Representative is made aware of and agrees to observe the
+              obligations contained herein. The Recipient may not disclose
+              Confidential Information to any other physical or legal person
+              except if agreed in writing by the Company.
+            </p>
+          </div>
+          <div class="col-6">
+            <p>
+              4 Non-competition, non-solicitation and non-recruitment 4.1 During
+              the duration of this Agreement, the Recipient will not, and will
+              ensure that the Recipient do not, without the prior written
+              consent of the Company, either directly or indirectly become
+              involved or participate in any capacity in any business,
+              activities, decisions, operations, undertakings or concerns which,
+              in the reasonable opinion of the Company: (a) conflict, or appear
+              to conflict, with the interests of the Company; and (b) are in
+              competition with, or are of a similar nature to, the activities of
+              the Company 4.2 In connection with the Recipient considering the
+              Collaboration, the Recipient and its Representatives shall not
+              contact any of the Company’s employees, suppliers or customers
+              without the Company’s prior written consent. 4.3 The Recipient
+              shall not solicit or entice or endeavour to solicit or entice any
+              existing supplier, customer or other contract or trading party of
+              the Company to terminate or alter their business relationship with
+              the Company. This clause 4.3 shall remain in force during the
+              negotiations regarding the Collaboration and for 12 months after
+              the termination of such negotiations if the negotiations are not
+              successful.
+              
+              <p>
+                4.4 The Recipient shall not solicit or entice or
+              endeavour to solicit or entice any directors, employees or other
+              representatives of the Company, or consultants delivering services
+              to the Company, to leave their current employment. This clause
+              shall remain in force during the negotiations regarding the
+              Collaboration and for 12 months after the termination of such
+              negotiations if the negotiations are not successful. 4.6 The
+              Recipient acknowledges that the covenants in respect of
+              non-competition contained in this clause are fair and reasonable
+              and that the Company is relying upon this acknowledgement in
+              entering into this agreement. The Recipient will ensure that the
+              Recipients directors, employees and agents will make the same
+              acknowledgement.
+              </p>
+              <p>
+                4.7 The Recipient agrees and acknowledges that
+              should the Recipient take over or acquire any client from the
+              Company during its term of engagement or within (12) twelve months
+              after the termination of its engagement with the Company the
+              Recipient will be required to pay compensation to the Company in
+              respect of those clients. The calculation for the compensation
+              shall be equal to one dollar ($1.00) for every dollar of gross
+              fees charged to that client for the previous twelve months. At the
+              Company’s discretion allowances may be made in respect of clients
+              introduced to the Company by the Recipient. This clause shall not
+              merge on completion of this Agreement. had direct dealings in the
+              course of, or in connection with, the duration of the NDA.
+              </p>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style lang="scss">
+.content {
+  background-color: #d7d7d7;
+}
+h3 {
+  color: $mainly_blue;
+  font-size: $font62;
+  text-transform: uppercase;
+  line-height: 68px;
+  @include NeoSans(900);
+}
+
+p {
+  font-size: $font16;
+  line-height: 24px;
+  @include NeoSans(400);
+  color: $charcoal;
+}
+.heroServices {
+  .heroFlag {
+    padding: 100px;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-image: url(~assets/images/ktv_dronepilot.jpg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    &:after {
+      content: "";
+      position: absolute;
+      inset: 0;
+      background-color: $black;
+      opacity: 0.4;
+    }
+  }
+  .text-box {
+    padding: 100px;
+    background: #e4e7ed !important;
+    p {
+      color: $primary !important;
+      margin-bottom: 20px;
+    }
+  }
+}
+
+.titleText {
+  position: relative;
+  z-index: 5;
+  h2 {
+    font-size: $font62;
+    color: $white;
+    margin-bottom: 20px;
+    text-transform: uppercase;
+  }
+  p {
+    font-size: $font42;
+    text-transform: uppercase;
+    color: $white;
+    line-height: 52px;
+  }
+}
+
+.method {
+  background: $primary;
+  color: $white;
+  padding: 240px 0 100px 0;
+  ol {
+    li {
+      margin-bottom: 20px;
+    }
+  }
+  .text-box {
+    h3 {
+      color: $white;
+    }
+  }
+  h4 {
+    font-size: 1.25rem;
+    color: $white;
+    line-height: 1.4;
+    margin-bottom: 0.9rem;
+  }
+  p {
+    color: $white;
+    margin-bottom: 0.9rem;
+  }
+}
+.standardSolar {
+  background: $primary;
+  padding: 100px 0px 100px 0px;
+  .text-box {
+    p,
+    h3 {
+      color: #e4e7ed;
+      margin-bottom: 20px;
+    }
+  }
+}
+.standardSolar {
+  &.aboutSetting {
+    position: relative;
+    background: $blue;
+
+    .shape {
+      fill: $primary;
+      position: absolute;
+      top: 100%;
+      left: 0;
+      right: 0;
+      height: 100px;
+      width: 100%;
+    }
+
+    .settingStabdardBox {
+      height: 100%;
+    }
+
+    .text-box {
+      h3,
+      p {
+        color: $white;
+        margin-bottom: 0.9rem;
+      }
+    }
+  }
+}
+
+.futureDevelopment {
+  position: relative;
+  background: $whiteSecondary !important;
+
+  .shape {
+    fill: $blue;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 46px;
+    width: 100%;
+  }
+
+  .text-box {
+    h3 {
+      color: $primary !important;
+    }
+
+    h4 {
+      font-weight: normal;
+      text-transform: uppercase;
+      font-size: $font42;
+      color: $primary !important;
+    }
+
+    ul {
+      padding-left: 20px;
+      color: $primary !important;
+
+      li {
+        list-style: disc;
+        font-size: $font16;
+      }
+    }
+  }
+}
+
+// responsive
+@import "~assets/scss/pages.scss/responsive.scss";
+</style>
+
+<script></script>
