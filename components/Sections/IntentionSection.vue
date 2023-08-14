989678cf416604876a92d825ce22c9e89ebb2750
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-08-15 00:24:45 +0300

    🌓 Little better
    - and little worse

--- components/Sections/IntentionSection.vue
+++ components/Sections/IntentionSection.vue
@@ -1,20 +1,16 @@
 <template>
-    <section class="intention-section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<div class="intention-left">
-							<h3>{{ $t("OUR_INTENTION") }}</h3>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="intention-right">
-							<h4>{{ $t("OUR_MAIN_GOAL_IS_TO_HELP_OTHERS_AND_MAKE_WORKING_LIFE_BETTER_FOR_EVERYONE") }}</h4>
-							<p>{{ $t("WE_WANT_TO_HELP_MAKE_WORKDAYS") }}</p>
-							<p>{{ $t("THESE_ARE_PHYSICALLY_DEMANDING_JOBS") }}</p>
-						</div>
-					</div>
+	<section class="intention-section text-lg py-16 md:py-32 bg-gray-800 text-white font-neo-sans">
+		<div class="container">
+			<div class="grid grid-cols md:grid-cols-2 gap-8">
+				<div class="flex justify-center items-center">
+					<h3 class="text-6xl font-bold">{{ $t("OUR_INTENTION") }}</h3>
+				</div>
+				<div class="intention-right space-y-3">
+					<h4 class="text-2xl font-bold mb-5">{{ $t("OUR_MAIN_GOAL_IS_TO_HELP_OTHERS_AND_MAKE_WORKING_LIFE_BETTER_FOR_EVERYONE") }}</h4>
+					<p>{{ $t("WE_WANT_TO_HELP_MAKE_WORKDAYS") }}</p>
+					<p>{{ $t("THESE_ARE_PHYSICALLY_DEMANDING_JOBS") }}</p>
 				</div>
 			</div>
-		</section>
+		</div>
+	</section>
 </template>
