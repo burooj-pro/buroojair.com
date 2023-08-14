5d156516cee2c84e22177379f8a6b32d4d687985
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-08-14 10:09:09 +0300

    sectioning sub pages

---
+++ components/Sections/FacadeCleaningAdvantagesSection.vue
@@ -0,0 +1,95 @@
+<template>
+	<section class="advantages">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8">
+					<div class="text-box">
+						<h3>{{ $t("ADVANTAGES_OF_GLASS_AND_FACADE_CLEANING_WITH_BUROOJ_AIR_DRONE") }}</h3>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<div class="text-box">
+						<h4>{{ $t("IMPORTANT_KEY_FACTS") }}</h4>
+						<p>
+							<strong>1000 {{ $t("SQM_HOUR") }}</strong
+							><br />{{ $t("CAPACITY_FACADE_CLEANING") }}
+						</p>
+						<p>
+							<strong>1500 {{ $t("SQM_HOUR") }}</strong
+							><br />{{ $t("CAPACIY_WINDOW_CLEANING") }}
+						</p>
+						<p>
+							<strong>{{ $t("HEIGHT") }} 100+ {{ $t("METER") }}</strong
+							><br />{{ $t("DRONE_FULLY_OPERATIONAL") }}
+						</p>
+						<p>
+							<strong>{{ $t("FLYTIME") }} 24 {{ $t("HOUR") }}</strong
+							><br />{{ $t("BATTERY_CASE_BATTERIES") }}
+						</p>
+						<p>
+							<strong>7 {{ $t("M_SEC") }}</strong
+							><br />{{ $t("WORKS_IN_MAXIMUM_WINDS") }} 7 {{ $t("M_SEC") }}. {{ $t("UP_TO") }} 15 {{ $t("M_SEC") }}
+							{{ $t("WITHOUT_EQUIPMENT") }}
+						</p>
+						<p>
+							<strong>{{ $t("UP_TO") }} 180 {{ $t("BAR_PRESSURE") }}</strong
+							><br />{{ $t("CAPACITY_WATER_PRESSURE") }}
+						</p>
+						<p>
+							<strong>{{ $t("AIRSENSE_ADS") }}</strong
+							><br />{{ $t("ADVANCED_FLIGHT_COLLISION") }}
+						</p>
+						<p>
+							<strong>{{ $t("SELF_HEATING_BATTERY") }}</strong
+							><br />{{ $t("EFFICIENT_BATTERY_USE") }}
+						</p>
+						<p>
+							<strong>-20°C {{ $t("TO") }} 50°C</strong><br />{{ $t("WORKING_TEMPERATURE_RANGE") }}
+						</p>
+						<p>
+							<strong>{{ $t("IP_45") }}</strong
+							><br />{{ $t("PROTECTED_AGAINST_PRESSURE") }}
+						</p>
+						<!-- <p>
+                <strong>1000 sqm/hour</strong><br />Capacity facade cleaning
+              </p>
+              <p>
+                <strong>1500 sqm/hour</strong><br />Capacity window cleaning
+              </p>
+              <p>
+                <strong>Height 100+ meter</strong><br />When the drone is fully
+                operational
+              </p>
+              <p>
+                <strong>Flytime 24 hour</strong><br />With battery case with 8
+                batteries
+              </p>
+              <p>
+                <strong>7 m/sec</strong><br />Works in maximum winds of 7 m/s.
+                Up to 15 m/s without equipment
+              </p>
+              <p>
+                <strong>Up to 180 bar pressure</strong><br />Capacity water
+                pressure
+              </p>
+              <p>
+                <strong>AirSense ADS-B</strong><br />Advanced flight / collision
+                warning system
+              </p>
+              <p>
+                <strong>Self-heating battery</strong><br />Provides efficient
+                battery use
+              </p>
+              <p>
+                <strong>-20°C to 50°C</strong><br />Working temperature range
+              </p>
+              <p>
+                <strong>IP 45</strong><br />Protected against low pressure water
+                jets
+              </p> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
