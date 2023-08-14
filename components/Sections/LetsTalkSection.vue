72f58ddd8f4fb26721d31148947e7e928d25115c
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-08-14 09:40:37 +0300

    🪓 More CSS and Package cleaning
    -Sectioning
    -SCSS cleaning
    -removed fiber, who needs that?

---
+++ components/Sections/LetsTalkSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="let-talk-section">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-7">
+						<div class="let-talk-left z-2">
+							<h4>{{ $t("FRANCHISE") }}</h4>
+							<p>{{ $t("OPPORTUNITIES_AVAILABLE") }}</p>
+						</div>
+					</div>
+					<div class="col-md-5">
+						<div class="let-talk-right z-2">
+							<p>
+								{{ $t("TAKE_PART_IN_THE_MOST_INNOVATIVE_BUSINESS_IDEA") }}
+							</p>
+							<a href="#" class="btn btn-secondary btn-blue">
+								<i class="bi bi-arrow-right fs-5 me-2"></i>
+								{{ $t("LET_S_TALK") }}
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+</template>
