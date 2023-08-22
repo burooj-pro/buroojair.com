3e407ebc129b7146cbdf6d9ae831a6a657272485
Mutlaq Aldhbuiub <matulkn@gmail.com>
2023-08-22 15:24:13 +0300

    Removing 's', Adding map to contact us

--- components/Sections/Contact.vue
+++ components/Sections/Contact.vue
@@ -17,6 +17,12 @@
 							<li>{{ $t('PHONE') }}: +966566639994</li>
 						</ul>
 					</div>
+
+
+					<div>
+						<!-- Embed map -->
+						<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1264.5133142299962!2d50.22489287392942!3d26.30759055407174!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e49e9a50968d01d%3A0x2bd0cd968fddc405!2zRWZmaWNpZW5jeSBDZW50ZXIgfCDZhdix2YPYsiDYp9mE2YPZgdin2KHYqQ!5e0!3m2!1sen!2ssa!4v1692706659408!5m2!1sen!2ssa" width="400" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+					</div>
 				</div>
 
 				<div class="pipedriveWebForms p-8" data-pd-webforms="https://webforms.pipedrive.com/f/6coemjWsjuoqlhJqDxw5vARg9RKtlXs9Rk7xeP0SZJLEXlmO9hyO7YDdWfq6IcrYLV">
