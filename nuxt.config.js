7785edab9320ef473fcca00877903ac7456ec8e0
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2025-05-21 10:09:35 +0300

    tiktok pixel

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,9 +20,33 @@ export default {
 			{ hid: 'twitter:title', name: 'twitter:title', content: 'Burooj Air' },
 			{ hid: 'twitter:description', name: 'twitter:description', content: 'Burooj Air is the first Saudi company licensed for drone cleaning services, offering innovative and eco-friendly solutions for high-rise facade cleaning and more.' },
 			{ hid: 'twitter:image', name: 'twitter:image', content: 'https://buroojair.com/images/BuroojAir.png' },
-
 			{ name: 'format-detection', content: 'telephone=no' },
 		],
+		script: [
+			{
+				hid: 'tiktok-pixel',
+				innerHTML: `!function (w, d, t) {
+				  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
+				  ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+				  ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+				  for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq, ttq.methods[i]);
+				  ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
+				  ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;
+				  ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};
+				  var s=d.createElement("script");s.type="text/javascript";s.async=!0;
+				  s.src=r+"?sdkid="+e+"&lib="+t;
+				  var f=d.getElementsByTagName("script")[0];
+				  f.parentNode.insertBefore(s,f)};
+				  ttq.load("D0M466RC77U6GBUFK3M0");
+				  ttq.page();
+				}(window, document, 'ttq');`,
+				type: 'text/javascript',
+				charset: 'utf-8'
+			}
+		],
+		__dangerouslyDisableSanitizersByTagID: {
+			'tiktok-pixel': ['innerHTML']
+		}
 	},
 
 	css: ['@/assets/scss/main.scss', 'animate.css/animate.min.css'],
