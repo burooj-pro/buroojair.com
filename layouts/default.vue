88c6adaac2458518e76bed5301ed896675770f7a
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2025-05-21 10:18:48 +0300

    gtm

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,23 @@
 <template>
-	<div :dir="this.$i18n.localeProperties.dir" class="relative">
+	<div :dir="$i18n.localeProperties.dir" class="relative">
+		<!-- Google Tag Manager (noscript) -->
+		<noscript>
+			<iframe
+				src="https://www.googletagmanager.com/ns.html?id=GTM-55SGMJF5"
+				height="0"
+				width="0"
+				style="display:none;visibility:hidden"
+			></iframe>
+		</noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
 		<Nuxt />
 	</div>
 </template>
 
 <script>
 import { initCollapses } from 'flowbite'
+
 export default {
 	mounted() {
 		initCollapses()
@@ -23,7 +35,8 @@ export default {
 				{
 					hid: 'description',
 					name: 'description',
-					content: 'We aim to contribute to a safer work day for people who work at great heights with lifts, scaffolding and for facade climbers.',
+					content:
+						'We aim to contribute to a safer work day for people who work at great heights with lifts, scaffolding and for facade climbers.',
 				},
 			],
 		}
--- nuxt.config.js
+++ nuxt.config.js
@@ -52,6 +52,16 @@ export default {
 	  ],
 	  script: [
 		{
+			hid: 'gtm',
+			innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			  })(window,document,'script','dataLayer','GTM-55SGMJF5');`,
+			type: 'text/javascript',
+			charset: 'utf-8'
+		  },
+		{
 		  hid: 'tiktok-pixel',
 		  innerHTML: `!function (w, d, t) {
 			w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
@@ -102,6 +112,7 @@ export default {
 		  },
 	  ],
 	  __dangerouslyDisableSanitizersByTagID: {
+		gtm: ['innerHTML'],
 		'tiktok-pixel': ['innerHTML'],
 		'snap-pixel': ['innerHTML'],
 		'meta-pixel': ['innerHTML'],
